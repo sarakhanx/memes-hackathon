a6e4cf1fb2f00b2823b80241c5aa8d291832cb9e
sarakhanX <sarawut.khan@hotmail.com>
2024-05-09 16:27:31 +0700

    fix layout

--- src/components/MaxWidth.tsx
+++ src/components/MaxWidth.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MaxWidth = ({ children }: Readonly<{ children: React.ReactNode }>) => (
-  <div className="max-w-3xl mx-auto px-4">
+  <div className="max-w-3xl mx-auto px-4 w-full">
     {children}
   </div>
 );
