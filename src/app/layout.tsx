2ec1898bf0f8f3a5ea0b98ac02b86152a7ef3b79
sarawut khantiyoo <87906315+sarakhanx@users.noreply.github.com>
2024-05-15 10:39:35 +0700

    fixed og img path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Programmer memes",
   description: "inspired by www.nasa.gov/content/goddard/what-did-hubble-see-on-your-birthday",
+  openGraph: {
+    images: [
+      {
+        url: '/opengraph-image',
+        width: 1200,
+        height: 630,
+        alt: 'โปรแกรมเมอร์ Quote',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
